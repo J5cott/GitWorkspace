0a4010b503714e4220a0143e3d38e9ec199bdf21
J5cott <justin.scot7@gmail.com>
2015-10-09 19:28:24 -0400

    BasicAlgebra class missing code when uploaded
    A commit in an attempt to resolve this issue.

--- Calculator/src/model/BasicAlgebra.java
+++ Calculator/src/model/BasicAlgebra.java
@@ -1,5 +1,25 @@
 package model;
 
 public class BasicAlgebra {
+	
+private double[] numbers;
 
+public void BasicAlgebra(double[]input){
+	setNumbers(input);
+}
+
+public void setNumbers(double[] num){
+	int i = 0;
+	for(double d : num){
+		numbers[i] = d;
+	}
+}
+
+public double[] getNumbers(){
+	return numbers;
+}
+
+public double addAll(){
+	
+}
 }
