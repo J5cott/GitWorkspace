5f1046cfcf2cdb25b84443d69887c8118848645e
nicholasH <nicholas.hoover@hope.edu>
2015-10-10 16:39:32 -0400

    Added clear method in input class & inputTest class undone

---
+++ Calculator/src/model/InputTest.java
@@ -0,0 +1,22 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class InputTest {
+
+	@Before
+	public void setUp() throws Exception {
+		
+		
+		
+	}
+
+	@Test
+	public void testCalucate() {
+		fail("Not yet implemented");
+	}
+
+}
