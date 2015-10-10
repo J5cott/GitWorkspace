8a2bff6d310d2e498dce11bcb3a922a3904d6295
nicholasH <nicholas.hoover@hope.edu>
2015-10-10 15:37:44 -0400

    Did the input class && the basicAlgebra class

--- Calculator/src/model/BasicAlgebra.java
+++ Calculator/src/model/BasicAlgebra.java
@@ -2,62 +2,18 @@ package model;
 import java.util.*;
 
 public class BasicAlgebra {
-	
-private double[] numbers;
 
-public BasicAlgebra(double[]input){
-	numbers = input;
+	
+public static double add(double input1, double input2){
+	
+	
+	return  input1 + input2;
 }
 
-public void setNumbers(double[] num){
-	int i = 0;
-	for(double d : num){
-		numbers[i] = d;
-	}
+
+public static double substract(double input1,double input2){
+	return input1 + input2;
 }
 
-public double[] getNumbers(){
-	return numbers;
-}
 
-/**
- * Adds all of the numbers stored in a provided list
- * @param list The list with the numbers to be added
- * @return the sum of 
- */
-public double addAll(double[] list){
-	
-	double returnnum = 0;
-	
-
-	if(list.length >= 1){
-	for(double d : list){
-		returnnum += d;
-	}
-	}
-
-	return returnnum;
-}
-/**
- * Subtracts all of the values in the list from the first value in the list.
- * @param list The list with the values to be subtracted
- * @return The first value after all the values after it in the list have subtracted from it.
- */
-public double subAll(double[] list){
-	double returnnum = 0;
-	
-	int i = 0;
-	for( double elem : list){
-		
-		
-		if(i == 0){
-			returnnum = elem;
-			i = 1;
-		} else{
-			returnnum -= elem;
-		}
-	}
-	
-	return returnnum;
-}
 }
--- Calculator/src/model/Input.java
+++ Calculator/src/model/Input.java
@@ -3,55 +3,49 @@ import java.util.*;
 
 public class Input {
 	
-private double[] input = new double[100];
 
-int indexoflastelement;
+private double lastnumber;
 
-/**
- * A constructor that takes a single number.
- * @param d The number to initialize the ArrayList.
- */
-public Input(double d){
-	input[0] = d;
-}
-
-/**
- * A constructor that takes an ArrayList.
- * @param input The ArrayList to initialize input.
- */
-public Input(double[] input){
-	this.input = input;
-	
-	int i = 0;
-}
-
-/**
- * This method adds a value to the input array.
- * @param d The value to add to the end of the nonzero values in the array.
- */
-public void addInput(double d){
-	for(double elem : input){
-		if(elem == 0.0){
-			elem = d;
+	public double calucate(Stack<String> math){
+		
+		while(math.isEmpty() == false){
+			String var1 = math.pop();
+		
+			if(var1 == "+"){
+				
+				String var2 = math.pop();
+				
+				
+				double input2 = Double.parseDouble(var2);
+				
+				lastnumber = BasicAlgebra.add(lastnumber,input2);
+				
+			}
+			else{
+				
+				double input1 = Double.parseDouble(var1);
+				String function = math.pop();
+				double input2 = Double.parseDouble(math.pop());
+				
+				switch(function){
+					
+					case("+"):
+						lastnumber = BasicAlgebra.add(input1,input2);
+						break;
+					case("-"):
+						lastnumber = BasicAlgebra.substract(input1,input2);
+						
+					
+				}
+				
+			}
+		
 		}
+		
+		return lastnumber;
 	}
-}
 
-/**
- * This method sets the input variable to
- * the provided list
- */
-public void setInput(double[] d){
-	input  = d;
-}
-
-/**
- * This method returns the stored input
- * @return An ArrayList<Double>
- */
-public double[] getInput(){
-	return input;
-}
-
-
+	
+	
+	
 }
